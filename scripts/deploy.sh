f17f8cdbe99c24bca0fea2ddd42c5672e1614599
5seonjae <5seonjae.dev@gmail.com>
2025-07-31 22:37:43 +0900

    fix(deploy) - 환경 변수 설정 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,14 +18,15 @@ echo "▶ deploy new JAR"
 cp "$BUILD_PATH" "$APP_DIR/"
 cd "$APP_DIR"
 
-export jwt.secret
-export jwt.expiration-ms
-export kakao.client-id
-export kakao.auth-url
-export kakao.api-url
-export kakao.redirect-uri
-export kakao.template-id
+nohup java -jar "$JAR_NAME" \
+  --spring.profiles.active=prod \
+  --jwt.secret="${JWT_SECRET}" \
+  --jwt.expiration-ms="${JWT_EXPIRATION_MS}" \
+  --kakao.client-id="${KAKAO_CLIENT_ID}" \
+  --kakao.auth-url="${KAKAO_AUTH_URL}" \
+  --kakao.api-url="${KAKAO_API_URL}" \
+  --kakao.redirect-uri="${KAKAO_REDIRECT_URI}" \
+  --kakao.template-id="${KAKAO_TEMPLATE_ID}" \
+  > "$APP_DIR/app.log" 2>&1 &
 
-nohup java -jar "$JAR_NAME" --spring.profiles.active=prod \
-> "$APP_DIR/app.log" 2>&1 &
 echo "▶ started! (bg)"

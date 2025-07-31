6d92d8037e8f042dd06cb89c48174c57c44464e6
5seonjae <5seonjae.dev@gmail.com>
2025-07-31 21:00:55 +0900

    fix(deploy.sh) - 환경 변수 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -18,6 +18,14 @@ echo "▶ deploy new JAR"
 cp "$BUILD_PATH" "$APP_DIR/"
 cd "$APP_DIR"
 
+export JWT_SECRET
+export JWT_EXPIRATION_MS
+export KAKAO_CLIENT_ID
+export KAKAO_API_URL
+export KAKAO_AUTH_URL
+export KAKAO_REDIRECT_URI
+export KAKAO_TEMPLATE_ID
+
 nohup java -jar "$JAR_NAME" --spring.profiles.active=prod \
 > "$APP_DIR/app.log" 2>&1 &
 echo "▶ started! (bg)"
